3d20f0e4dc4618a759915c23141d995e5fa11314
xxc <17770092259@163.com>
2021-04-10 21:25:55 +0800

    添加文件存取类

---
+++ common/src/main/java/com/originit/common/utils/FileUDUtil.java
@@ -0,0 +1,253 @@
+package com.originit.common.utils;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.URLEncoder;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.UUID;
+
+@EnableConfigurationProperties(FileUDUtil.FileProperties.class)
+@Component
+@Slf4j
+public class FileUDUtil {
+
+    @Data
+    @ConfigurationProperties("custom.fileupload")
+    public static class FileProperties {
+        private String path = "\\images";
+
+        private boolean external = false;
+    }
+
+
+
+    public static Logger logger = LoggerFactory.getLogger(FileUDUtil.class);
+
+    public static String PATH = "\\images";
+
+    // 是否在项目外部
+    public static Boolean EXTERNAL = false;
+
+    @Autowired
+    public void setProperties(FileProperties properties){
+        FileUDUtil.EXTERNAL = properties.external;
+        FileUDUtil.PATH = properties.path;
+    }
+
+
+    /**
+     * 处理下载文件时的文件名中文乱码问题，兼容浏览器
+     * @param filename 源文件名
+     * @param agent 浏览器的user-agent
+     */
+    private static String resolveDownloadFileName(String filename, String agent){
+        try {
+            if (agent.contains("MSIE"))
+            {
+                // IE浏览器
+                filename = URLEncoder.encode(filename, "utf-8");
+                filename = filename.replace("+", " ");
+            }
+            else if (agent.contains("Firefox"))
+            {
+                // 火狐浏览器
+                filename = new String(filename.getBytes(), "ISO8859-1");
+            }
+            else if (agent.contains("Chrome"))
+            {
+                // google浏览器
+                filename = URLEncoder.encode(filename, "utf-8");
+            }
+            else
+            {
+                // 其它浏览器
+                filename = URLEncoder.encode(filename, "utf-8");
+            }
+        } catch (Exception e) {
+            logger.error(ExceptionUtil.buildErrorMessage(e));
+            throw new IllegalStateException("文件名编码异常");
+        }
+        return filename;
+    }
+
+    /**
+     * 将path的文件转换为ResponseEntity
+     * @param code 文件所在文件夹路径
+     * @param filename 文件名
+     * @param agent 浏览器的user-agent
+     */
+    public static void downloadFile(String code, String filename, String agent, HttpServletResponse resp) {
+        String realPath = new String(Base64.getDecoder().decode(code), StandardCharsets.UTF_8);
+        if (filename == null) {
+            filename = realPath.substring(realPath.lastIndexOf(File.separator) + 1);
+        }
+        log.info("【初始化文件上传工具类】current download file name is {}", filename);
+        downloadFileWithPath("",realPath,filename,agent,resp);
+    }
+
+    /**
+     * 获取对应code的文件
+     * @param code 上传文件生成的code
+     */
+    public static File getFile (String code) {
+        if (code == null) {
+            return null;
+        }
+        String realPath = new String(Base64.getDecoder().decode(code), StandardCharsets.UTF_8);
+        // 获取目录下的资源
+        return getFileByPath(realPath);
+    }
+
+    public static void downloadFileWithPath (String relativePath,String realPath, String filename, String agent, HttpServletResponse resp) {
+        try {
+            File file;
+            // 获取目录下的资源,如果指定了相对的路径就使用相对路径
+            if (relativePath == null || relativePath.trim().isEmpty()){
+                file = getFileByPath(realPath);
+            } else {
+                file = new File(FileUDUtil.class.getClassLoader().getResource(relativePath).getPath(), realPath);
+            }
+
+            resp.reset();
+//          // 让浏览器显示下载文件对话框
+            resp.setContentType(MediaType.parseMediaType(Files.probeContentType(Paths.get(file.getAbsolutePath()))).getType());
+            resp.setCharacterEncoding("utf-8");
+            resp.setContentLength((int) file.length());
+            resp.setHeader("Content-Disposition", "attachment;filename="+resolveDownloadFileName(filename,agent));
+            byte[] buff = new byte[1024];
+            BufferedInputStream bis = null;
+            OutputStream os = null;
+            try {
+                os = resp.getOutputStream();
+                bis = new BufferedInputStream(new FileInputStream(file));
+                int i = 0;
+                while ((i = bis.read(buff)) != -1) {
+                    os.write(buff, 0, i);
+                    os.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalStateException("文件下载异常");
+        }
+    }
+
+    /**
+     * 获取默认的头像
+     * @return 默认头像
+     */
+    public static File getDefaultHeadImg () {
+        return new File(FileUDUtil.class.getClassLoader().getResource("images").getPath(), "/default_logo.jpg");
+    }
+
+
+    /**
+     * 通过文件名获取hash
+     * @param filename 文件名
+     * @return 路径
+     */
+    private static String getPath (String filename) {
+        //得到hashCode
+        int hashcode = filename.hashCode();
+        //得到名为1到16的下及文件夹
+        int dir1 = hashcode & 0xf;
+        //得到名为1到16的下下及文件夹
+        int dir2 = (hashcode & 0xf0) >> 4;
+        //得到文件路径
+        String dir = PATH + File.separator + dir1 + File.separator + dir2 + File.separator +
+                UUID.randomUUID().toString().replace("-","") +
+                filename.substring(filename.lastIndexOf("."));
+        return dir;
+    }
+    /***
+     * 上传文件
+     * @param inputStream 文件的输入流
+     * @param filename 文件名
+     * @return 文件的编码
+     */
+    public static String saveFile(InputStream inputStream, String filename) {
+        try {
+            String path = getPath(filename);
+            mkdirIfNotExist(path);
+            File file = getFileByPath(path);
+            // TODO 修改
+            FileCopyUtils.copy(inputStream,new FileOutputStream(file));
+            log.info("save file at the position path in {}",file.getAbsolutePath());
+            // 返回base64编码
+            return  Base64.getEncoder().encodeToString(path.getBytes(StandardCharsets.UTF_8));
+        }catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalStateException("文件上传异常");
+        }
+    }
+
+    /**
+     * 创建文件夹
+     * @param path 路径
+     */
+    private static void mkdirIfNotExist(String path) {
+        File file = getFileByPath(path.substring(0,path.lastIndexOf(File.separator)));
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+    }
+
+    /**
+     * 获取文件的系统url
+     * @param code 编码code
+     * @return
+     */
+    public static String getSystemURL (String code) {
+        if (code == null) {
+            return null;
+        }
+        try {
+            final String hostAddress = InetAddress.getLocalHost().getHostAddress();
+            String url = "http://" + hostAddress + "/union" + "/resource/file/" + code;
+            logger.warn("the ip is {}",url);
+            return url;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            logger.error("the ip is null,error {}",ExceptionUtil.buildErrorMessage(e));
+        }
+        return null;
+    }
+
+    /**
+     * 统一的获取文件
+     * @param path 文件路径
+     * @return 文件
+     */
+    private static File getFileByPath(String path){
+        if (EXTERNAL) {
+            return new File(path);
+        }
+        return  new File(FileUDUtil.class.getClassLoader().getResource("").getPath(), path);
+    }
+}
---
+++ common/src/main/java/com/originit/common/utils/ResourceUtil.java
@@ -0,0 +1,84 @@
+package com.originit.common.utils;
+
+import cn.hutool.core.io.IoUtil;
+import com.originit.common.exception.InternalServerException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.*;
+import org.springframework.web.multipart.MultipartFile;
+import sun.misc.BASE64Encoder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
+@Slf4j
+public class ResourceUtil {
+
+    public static void getFile2Response(String path, String filename, String agent, HttpServletResponse response) {
+        try {
+            ResponseEntity<byte[]> r = getFile2BytesInternal(path, filename, agent);
+            response.setStatus(r.getStatusCode().value());
+            r.getHeaders().forEach((s, strings) -> {
+                if (strings.size() == 1){
+                    response.setHeader(s,strings.get(0));
+                } else {
+                    for (String string : strings) {
+                        response.addHeader(s,string);
+                    }
+                }
+            });
+            IoUtil.write(response.getOutputStream(),false,r.getBody());
+        } catch (Exception e) {
+            log.error("exception:{}\r\nmsg:{}\r\ntrace:{}",e.getClass(),e.getMessage(), cn.hutool.core.exceptions.ExceptionUtil.stacktraceToString(e));
+            throw new InternalServerException();
+        }
+    }
+
+    public static ResponseEntity<byte[]> getFile2Bytes(String path, String filename, String agent, HttpServletResponse request) {
+        try {
+            return getFile2BytesInternal(path,filename,agent);
+        } catch (Exception e) {
+            log.error("exception:{}\r\nmsg:{}\r\ntrace:{}",e.getClass(),e.getMessage(),cn.hutool.core.exceptions.ExceptionUtil.stacktraceToString(e));
+            throw new InternalServerException();
+        }
+    }
+
+    private static ResponseEntity<byte[]> getFile2BytesInternal(String path,String filename,String agent) throws IOException {
+        // 获取目录下的资源
+        ClassPathResource cpr = new ClassPathResource(path+ File.separatorChar + filename);
+        // 获得请求头中的User-Agent
+        // 根据不同的客户端进行不同的编码
+        String filenameEncoder;
+        if (agent != null) {
+            if (agent.contains("MSIE")){
+                // IE浏览器
+                filenameEncoder = URLEncoder.encode(filename, "utf-8");
+                filenameEncoder = filenameEncoder.replace("+", " ");
+            }
+            else if (agent.contains("Firefox"))
+            {
+                // 火狐浏览器
+                BASE64Encoder base64Encoder = new BASE64Encoder();
+                filenameEncoder = "=?utf-8?B?" + base64Encoder.encode(filename.getBytes(Charset.forName("utf-8"))) + "?=";
+            }else { // 其它浏览器
+                filenameEncoder = URLEncoder.encode(filename, "utf-8");
+            }
+        }
+        else { // 其它浏览器
+            filenameEncoder = URLEncoder.encode(filename, "utf-8");
+        }
+        // 模板代码，装配ResponseEntity
+        InputStream input = cpr.getInputStream();
+        byte[] bytes = IoUtil.readBytes(input);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaTypeFactory.getMediaType(filename).orElse(MediaType.MULTIPART_FORM_DATA));
+        // 让浏览器显示下载文件对话框
+        headers.add("Content-Disposition", "attchement;filename="+filenameEncoder);
+        HttpStatus statusCode = HttpStatus.OK;
+        return new ResponseEntity<>(bytes, headers, statusCode);
+    }
+}
